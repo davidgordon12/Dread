83ced7cb33a8cc959851e93e5f91073606b26947
tehtko <moderndsnsam@gmail.com>
2022-08-02 00:42:37 -0400

    Start implementing cd

--- dread/src/main.rs
+++ dread/src/main.rs
@@ -1,7 +1,9 @@
 use std::io::stdin;
 use std::io::stdout;
 use std::io::Write;
+use std::path::Path;
 use std::process::Command;
+use std::env;
 
 static VERSION: f32 = 1.0;
 
@@ -9,16 +11,30 @@ fn main() {
     println!("Dread - Version {:.1}", VERSION);
     loop {
         print!("> ");
-        stdout().flush();
+        let _buffer = stdout().flush();
 
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
+        
+        let _command = input.trim().split_whitespace().next().unwrap();
+        let args = input.trim().split_whitespace();
 
-        let mut process = Command::new("powershell")
-            .arg(input.trim())
-            .spawn()
-            .unwrap();
+        match _command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                } 
+            },
+            _command => {
+                let mut process = Command::new(_command)
+                    .args(args)
+                    .spawn()
+                    .unwrap();
 
-        let _result = process.wait().unwrap();
+                let _result = process.wait().unwrap();
+            }
+        }
     }
 }
