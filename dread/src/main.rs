91eb4250d181bd6cf55e8ff6c3a40ca4f4477d6b
tehtko <moderndsnsam@gmail.com>
2022-08-02 21:57:23 -0400

    Add Linux support and QOL changes

--- dread/src/main.rs
+++ dread/src/main.rs
@@ -4,13 +4,16 @@ use std::io::Write;
 use std::path::Path;
 use std::process::Command;
 use std::env;
+use whoami;
 
 static VERSION: f32 = 1.0;
 
 fn main() {
     println!("Dread - Version {:.1}", VERSION);
     loop {
-        print!("> ");
+        print!(" {} at ", whoami::username());
+        get_directory();
+        print!(" > ");
         let _buffer = stdout().flush();
 
         let mut input = String::new();
@@ -18,18 +21,25 @@ fn main() {
         
         let _command = input.trim().split_whitespace().next().unwrap();
         let args = input.trim().split_whitespace();
-
+        
         match _command {
             "cd" => {
                  change_directory(&input);
             },
             _command => {
-                let mut process = Command::new("powershell")
-                    .args(args)
-                    .spawn()
-                    .unwrap();
-
-                let _result = process.wait().unwrap();
+                if cfg!(target_os = "windows"){
+                    let mut process = Command::new("powershell")
+                        .args(args)
+                        .spawn()
+                        .unwrap();
+                    let _result = process.wait().unwrap();
+                } else {
+                    let mut process = Command::new("sh")
+                        .args(args)
+                        .spawn()
+                        .unwrap();
+                    let _result = process.wait().unwrap();
+                }
             }
         }
     }
@@ -41,4 +51,10 @@ fn change_directory(input: &str) {
     if let Err(e) = env::set_current_dir(&root) {
         eprintln!("{}", e);
     }
+}
+
+fn get_directory() -> std::io::Result<()> {
+    let path = env::current_dir()?;
+    println!("{}", path.display());
+    Ok(())
 }
